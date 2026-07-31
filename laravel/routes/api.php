4b22ea5d846f72f98f15b2510a4f980573dc3a79
Safwan <fajarsafwan@gmail.com>
2026-08-01 01:01:25 +0700

    feat(category): create category module structure

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\Product\CategoryController;
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+
+
+});
