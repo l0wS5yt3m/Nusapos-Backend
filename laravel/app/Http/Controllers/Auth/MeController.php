55550722bde5aa1622f46f33ff805211d35a7653
Safwan <fajarsafwan@gmail.com>
2026-08-01 11:02:05 +0700

    feat(auth): add MeController

---
+++ laravel/app/Http/Controllers/Auth/MeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MeController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => 'Profile endpoint is ready.',
+        ]);
+    }
+}
