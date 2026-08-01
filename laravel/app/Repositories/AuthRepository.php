b91fbaad88187d674ab8cc3f01010c1e219cdeed
Safwan <fajarsafwan@gmail.com>
2026-08-01 11:02:16 +0700

    feat(auth): add AuthRepository

---
+++ laravel/app/Repositories/AuthRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+
+class AuthRepository
+{
+    public function findByEmail(string $email): ?User
+    {
+        return User::where('email', $email)
+            ->with('role')
+            ->first();
+    }
+}
