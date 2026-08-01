1211aa4909620b67d5efccb7ef0bc57b30fe338f
Safwan <fajarsafwan@gmail.com>
2026-08-01 11:02:54 +0700

    feat(auth): register authentication routes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,8 +1,27 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Product\CategoryController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\MeController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+
+});
+
+Route::prefix('v1')->group(function () {
+
+     Route::post('/login', LoginController::class);
+
+    Route::middleware('auth:sanctum')->group(function () {
+
+        Route::post('/logout', LogoutController::class);
+
+        Route::get('/me', MeController::class);
+
+    });
+
+});
